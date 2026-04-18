da599df4137368d63763e14d6674d28e80d2b99e
doduongphi <no1duongphi@gmail.com>
2026-04-18 21:54:20 +0700

    fix error in vercel

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -5,13 +5,16 @@ declare module "next-auth" {
   interface Session {
     user: {
       id: string;
-      role: string;
+      role: string | null;
     } & DefaultSession["user"];
   }
 
   interface User {
     id?: string;
-    role?: string;
+    role?: string | null;
+    name?: string | null;
+    email?: string | null;
+    image?: string | null;
   }
 }
 
